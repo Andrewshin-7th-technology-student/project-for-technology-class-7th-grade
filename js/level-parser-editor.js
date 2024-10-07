cb5465b3afb71aebd674a7620546eca19632ce65
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:38:04 -0700

    Create level-parser-editor.js

---
+++ js/level-parser-editor.js
@@ -0,0 +1,22 @@
+// level-parser-editor.js
+// Level parser editing and saving
+
+// Initialize level parser editor variables
+let levelParserEditor;
+
+// Initialize level parser editor
+function initLevelParserEditor() {
+  levelParserEditor = new LevelParserEditor();
+}
+
+// Edit level parser
+function editLevelParser() {
+  // Edit level parser
+  levelParserEditor.edit();
+}
+
+// Save level parser
+function saveLevelParser() {
+  // Save level parser
+  levelParserEditor.save();
+}
