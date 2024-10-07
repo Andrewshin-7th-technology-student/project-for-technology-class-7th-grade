c255ce9c6eb46eb1003e115653d0b796cb697ec8
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:41:44 -0700

    Create animation-manager-editor.js

---
+++ js/animation-manager-editor.js
@@ -0,0 +1,22 @@
+// animation-manager-editor.js
+// Animation manager editing and saving
+
+// Initialize animation manager editor variables
+let animationManagerEditor;
+
+// Initialize animation manager editor
+function initAnimationManagerEditor() {
+  animationManagerEditor = new AnimationManagerEditor();
+}
+
+// Edit animation manager
+function editAnimationManager() {
+  // Edit animation manager
+  animationManagerEditor.edit();
+}
+
+// Save animation manager
+function saveAnimationManager() {
+  // Save animation manager
+  animationManagerEditor.save();
+}
