d3885af4229f637eaa08d3a678dc510c3c74870f
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:47:24 -0700

    Create level-selector-editor-editor.js

---
+++ js/level-selector-editor-editor.js
@@ -0,0 +1,22 @@
+// level-selector-editor-editor.js
+// Level selector editor editing and saving
+
+// Initialize level selector editor editor variables
+let levelSelectorEditorEditor;
+
+// Initialize level selector editor editor
+function initLevelSelectorEditorEditor() {
+  levelSelectorEditorEditor = new LevelSelectorEditorEditor();
+}
+
+// Edit level selector editor
+function editLevelSelectorEditor() {
+  // Edit level selector editor
+  levelSelectorEditorEditor.edit();
+}
+
+// Save level selector editor
+function saveLevelSelectorEditor() {
+  // Save level selector editor
+  levelSelectorEditorEditor.save();
+}
