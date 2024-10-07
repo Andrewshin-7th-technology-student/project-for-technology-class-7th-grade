bae71be4ff737354abb0f8287f694bc4d5910a7e
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:40:08 -0700

    Create image-loader-editor.js

---
+++ js/image-loader-editor.js
@@ -0,0 +1,22 @@
+// image-loader-editor.js
+// Image loader editing and saving
+
+// Initialize image loader editor variables
+let imageLoaderEditor;
+
+// Initialize image loader editor
+function initImageLoaderEditor() {
+  imageLoaderEditor = new ImageLoaderEditor();
+}
+
+// Edit image loader
+function editImageLoader() {
+  // Edit image loader
+  imageLoaderEditor.edit();
+}
+
+// Save image loader
+function saveImageLoader() {
+  // Save image loader
+  imageLoaderEditor.save();
+}
