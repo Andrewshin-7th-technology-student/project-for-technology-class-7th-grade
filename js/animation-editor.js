7f3fb4b4fc162ea2c272fb51c4729302a074eda5
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:35:18 -0700

    Create animation-editor.js

---
+++ js/animation-editor.js
@@ -0,0 +1,22 @@
+// animation-editor.js
+// Animation editing and saving
+
+// Initialize animation editor variables
+let animationEditor;
+
+// Initialize animation editor
+function initAnimationEditor() {
+  animationEditor = new AnimationEditor();
+}
+
+// Edit animation
+function editAnimation() {
+  // Edit animation
+  animationEditor.edit();
+}
+
+// Save animation
+function saveAnimation() {
+  // Save animation
+  animationEditor.save();
+}
