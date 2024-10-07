791d2bdfc84eb229aa00007042f92742f7f31243
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:29:38 -0700

    Create sound-effect-player.js

---
+++ js/sound-effect-player.js
@@ -0,0 +1,16 @@
+// sound-effect-player.js
+// Sound effect player implementation
+
+// Initialize sound effect player variables
+let soundEffectPlayer;
+
+// Initialize sound effect player
+function initSoundEffectPlayer() {
+  soundEffectPlayer = new Audio();
+}
+
+// Play sound effect
+function playSoundEffect() {
+  // Play sound effect
+  soundEffectPlayer.play();
+}
