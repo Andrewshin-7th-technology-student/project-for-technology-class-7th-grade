766ff4c649048c19bb346b598b3a2cc43c45b191
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:42:16 -0700

    Create input-manager-editor.js

---
+++ js/input-manager-editor.js
@@ -0,0 +1,22 @@
+// input-manager-editor.js
+// Input manager editing and saving
+
+// Initialize input manager editor variables
+let inputManagerEditor;
+
+// Initialize input manager editor
+function initInputManagerEditor() {
+  inputManagerEditor = new InputManagerEditor();
+}
+
+// Edit input manager
+function editInputManager() {
+  // Edit input manager
+  inputManagerEditor.edit();
+}
+
+// Save input manager
+function saveInputManager() {
+  // Save input manager
+  inputManagerEditor.save();
+}
