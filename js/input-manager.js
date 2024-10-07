edd0ebf4359cac669f087ffa3ac9349d4e45bf6b
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:31:51 -0700

    Create input-manager.js

---
+++ js/input-manager.js
@@ -0,0 +1,22 @@
+// input-manager.js
+// Input handling and event management
+
+// Initialize input manager variables
+let inputManager;
+
+// Initialize input manager
+function initInputManager() {
+  inputManager = new InputManager();
+}
+
+// Handle input
+function handleInput() {
+  // Handle input
+  inputManager.handle();
+}
+
+// Manage events
+function manageEvents() {
+  // Manage events
+  inputManager.manage();
+}
