72138619864c5e891c412dd5243450a64d83f061
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:45:32 -0700

    Create collision-editor-editor.js

---
+++ js/collision-editor-editor.js
@@ -0,0 +1,8 @@
+// collision-editor-editor.js
+// Collision editor editing and saving
+
+// Initialize collision editor editor variables
+let collisionEditorEditor;
+
+// Initialize collision editor editor
+function initCollisionEditorEditor() {
