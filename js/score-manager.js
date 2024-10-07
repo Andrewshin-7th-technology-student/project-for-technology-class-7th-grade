f0e4fc985ee3b914618372bdd34ed87a01c4dbbf
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:27:33 -0700

    Create score-manager.js

---
+++ js/score-manager.js
@@ -0,0 +1,17 @@
+// score-manager.js
+// Score tracking and display
+
+// Initialize score variables
+let score = 0;
+
+// Update score
+function updateScore() {
+  // Update score
+  score += 10;
+}
+
+// Display score
+function displayScore() {
+  // Display score
+  console.log('Score: ' + score);
+}
