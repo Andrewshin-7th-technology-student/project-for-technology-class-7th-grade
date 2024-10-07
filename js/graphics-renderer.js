1665da19ae5d88837de680dc14adec162c3ab081
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:30:43 -0700

    Create graphics-renderer.js

---
+++ js/graphics-renderer.js
@@ -0,0 +1,22 @@
+// graphics-renderer.js
+// Graphics rendering and animation
+
+// Initialize graphics renderer variables
+let graphicsRenderer;
+
+// Initialize graphics renderer
+function initGraphicsRenderer() {
+  graphicsRenderer = new GraphicsRenderer();
+}
+
+// Render graphics
+function renderGraphics() {
+  // Render graphics
+  graphicsRenderer.render();
+}
+
+// Animate graphics
+function animateGraphics() {
+  // Animate graphics
+  graphicsRenderer.animate();
+}
