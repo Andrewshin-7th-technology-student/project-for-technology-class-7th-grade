4d1b33f9d43c0414e56231218168710ef7485ab2
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:29:16 -0700

    Create audio-player.js

---
+++ js/audio-player.js
@@ -0,0 +1,16 @@
+// audio-player.js
+// Audio player implementation
+
+// Initialize audio player variables
+let audioPlayer;
+
+// Initialize audio player
+function initAudioPlayer() {
+  audioPlayer = new Audio();
+}
+
+// Play audio
+function playAudio() {
+  // Play audio
+  audioPlayer.play();
+}
