2794247cca2e31030141eaba239c3b1dd36fa842
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:46:33 -0700

    Create input-editor-editor.js

---
+++ js/input-editor-editor.js
@@ -0,0 +1,22 @@
+// input-editor-editor.js
+// Input editor editing and saving
+
+// Initialize input editor editor variables
+let inputEditorEditor;
+
+// Initialize input editor editor
+function initInputEditorEditor() {
+  inputEditorEditor = new InputEditorEditor();
+}
+
+// Edit input editor
+function editInputEditor() {
+  // Edit input editor
+  inputEditorEditor.edit();
+}
+
+// Save input editor
+function saveInputEditor() {
+  // Save input editor
+  inputEditorEditor.save();
+}
