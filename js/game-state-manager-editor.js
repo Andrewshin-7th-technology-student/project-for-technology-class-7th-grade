f021c623f5394f05d418ea7f6357444dc427aeb4
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:42:31 -0700

    Create game-state-manager-editor.js

---
+++ js/game-state-manager-editor.js
@@ -0,0 +1,22 @@
+// game-state-manager-editor.js
+// Game state manager editing and saving
+
+// Initialize game state manager editor variables
+let gameStateManagerEditor;
+
+// Initialize game state manager editor
+function initGameStateManagerEditor() {
+  gameStateManagerEditor = new GameStateManagerEditor();
+}
+
+// Edit game state manager
+function editGameStateManager() {
+  // Edit game state manager
+  gameStateManagerEditor.edit();
+}
+
+// Save game state manager
+function saveGameStateManager() {
+  // Save game state manager
+  gameStateManagerEditor.save();
+}
