c02234e90d22a97e25ce6295de94b766f7807b65
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:33:56 -0700

    Create audio-editor.js

---
+++ js/audio-editor.js
@@ -0,0 +1,22 @@
+// audio-editor.js
+// Audio editing and saving
+
+// Initialize audio editor variables
+let audioEditor;
+
+// Initialize audio editor
+function initAudioEditor() {
+  audioEditor = new AudioEditor();
+}
+
+// Edit audio
+function editAudio() {
+  // Edit audio
+  audioEditor.edit();
+}
+
+// Save audio
+function saveAudio() {
+  // Save audio
+  audioEditor.save();
+}
