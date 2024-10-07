2e30d9b0c1fccfc56e7476468ca966177c9245e1
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:53:41 -0700

    Create graphics-editor-editor-editor.js

---
+++ js/graphics-editor-editor-editor.js
@@ -0,0 +1,22 @@
+// graphics-editor-editor-editor.js
+// Graphics editor editor editing and saving
+
+// Initialize graphics editor editor editor variables
+let graphicsEditorEditorEditor;
+
+// Initialize graphics editor editor editor
+function initGraphicsEditorEditorEditor() {
+  graphicsEditorEditorEditor = new GraphicsEditorEditorEditor();
+}
+
+// Edit graphics editor editor
+function editGraphicsEditorEditor() {
+  // Edit graphics editor editor
+  graphicsEditorEditorEditor.edit();
+}
+
+// Save graphics editor editor
+function saveGraphicsEditorEditor() {
+  // Save graphics editor editor
+  graphicsEditorEditorEditor.save();
+}
