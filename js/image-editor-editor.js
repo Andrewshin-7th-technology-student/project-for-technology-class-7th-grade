b4a1f0fb8b3fd4403182bcf0740790a990653b18
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:44:46 -0700

    Create image-editor-editor.js

---
+++ js/image-editor-editor.js
@@ -0,0 +1,22 @@
+// image-editor-editor.js
+// Image editor editing and saving
+
+// Initialize image editor editor variables
+let imageEditorEditor;
+
+// Initialize image editor editor
+function initImageEditorEditor() {
+  imageEditorEditor = new ImageEditorEditor();
+}
+
+// Edit image editor
+function editImageEditor() {
+  // Edit image editor
+  imageEditorEditor.edit();
+}
+
+// Save image editor
+function saveImageEditor() {
+  // Save image editor
+  imageEditorEditor.save();
+}
