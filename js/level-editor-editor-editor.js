9c018d269dc9d07a788c46850e1de13d599666cd
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:47:39 -0700

    Create level-editor-editor-editor.js

---
+++ js/level-editor-editor-editor.js
@@ -0,0 +1,22 @@
+// level-editor-editor-editor.js
+// Level editor editor editing and saving
+
+// Initialize level editor editor editor variables
+let levelEditorEditorEditor;
+
+// Initialize level editor editor editor
+function initLevelEditorEditorEditor() {
+  levelEditorEditorEditor = new LevelEditorEditorEditor();
+}
+
+// Edit level editor editor
+function editLevelEditorEditor() {
+  // Edit level editor editor
+  levelEditorEditorEditor.edit();
+}
+
+// Save level editor editor
+function saveLevelEditorEditor() {
+  // Save level editor editor
+  levelEditorEditorEditor.save();
+}
