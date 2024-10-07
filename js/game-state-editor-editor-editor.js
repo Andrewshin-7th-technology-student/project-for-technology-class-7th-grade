2bb2f8ada87f2a6aa3344fab67b377ccdef59f9b
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:54:50 -0700

    Create game-state-editor-editor-editor.js

---
+++ js/game-state-editor-editor-editor.js
@@ -0,0 +1,14 @@
+// game-state-editor-editor-editor.js
+// Game state editor editor editing and saving
+
+// Initialize game state editor editor editor variables
+let gameStateEditorEditorEditor;
+
+// Initialize game state editor editor editor
+function initGameStateEditorEditorEditor() {
+  gameStateEditorEditorEditor = new GameStateEditorEditorEditor();
+}
+
+// Edit game state editor editor
+function editGameStateEditorEditor() {
+  // Edit game state editor editor
