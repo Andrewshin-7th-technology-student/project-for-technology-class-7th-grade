dfa762aefd70f13edd041261d4ce4135e4e337d4
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:40:24 -0700

    Create graphics-renderer-editor.js

---
+++ js/graphics-renderer-editor.js
@@ -0,0 +1,22 @@
+// graphics-renderer-editor.js
+// Graphics renderer editing and saving
+
+// Initialize graphics renderer editor variables
+let graphicsRendererEditor;
+
+// Initialize graphics renderer editor
+function initGraphicsRendererEditor() {
+  graphicsRendererEditor = new GraphicsRendererEditor();
+}
+
+// Edit graphics renderer
+function editGraphicsRenderer() {
+  // Edit graphics renderer
+  graphicsRendererEditor.edit();
+}
+
+// Save graphics renderer
+function saveGraphicsRenderer() {
+  // Save graphics renderer
+  graphicsRendererEditor.save();
+}
