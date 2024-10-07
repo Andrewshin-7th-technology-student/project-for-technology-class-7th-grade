528b1996727b0a8de269247d5cd6d2601f8f9fdc
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-07 14:44:33 -0700

    Create sound-effect-editor-editor.js

---
+++ js/sound-effect-editor-editor.js
@@ -0,0 +1,22 @@
+// sound-effect-editor-editor.js
+// Sound effect editor editing and saving
+
+// Initialize sound effect editor editor variables
+let soundEffectEditorEditor;
+
+// Initialize sound effect editor editor
+function initSoundEffectEditorEditor() {
+  soundEffectEditorEditor = new SoundEffectEditorEditor();
+}
+
+// Edit sound effect editor
+function editSoundEffectEditor() {
+  // Edit sound effect editor
+  soundEffectEditorEditor.edit();
+}
+
+// Save sound effect editor
+function saveSoundEffectEditor() {
+  // Save sound effect editor
+  soundEffectEditorEditor.save();
+}
